9d2c46bb59c01571939b81856dcf038a0f1d0350
ajeeshRS <ajeeshrs79@gmail.com>
2024-10-21 19:17:56 +0530

    added initial landing page

---
+++ src/app/fonts/font.ts
@@ -0,0 +1,10 @@
+import { Karla, Inconsolata } from "next/font/google";
+
+export const karla = Karla({
+  weight: ["200", "400", "600", "700", "800"],
+  subsets: ["latin"],
+});
+export const inconsolata = Inconsolata({
+  weight: ["200", "400", "600", "700", "800"],
+  subsets: ["latin"],
+});
